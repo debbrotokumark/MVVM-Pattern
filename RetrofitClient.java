298cdd838f0eaf147b6f26d446cf6b814c9434c7
Debbroto Kumar Karmokar <136155528+debbrotokumark@users.noreply.github.com>
2025-11-15 16:18:50 +0600

    Create RetrofitClient.java

---
+++ RetrofitClient.java
@@ -0,0 +1,28 @@
+public class RetrofitClient {
+
+    private static final String BASE_URL = "https://yourdomain.com/api/";
+    private static Retrofit retrofit;
+
+    public static ApiService getApiService() {
+
+        if (retrofit == null) {
+
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(logging)
+                    .connectTimeout(30, TimeUnit.SECONDS)
+                    .readTimeout(30, TimeUnit.SECONDS)
+                    .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return retrofit.create(ApiService.class);
+    }
+}
